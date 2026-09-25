2eaa98f9a02647921a9113803f22ac1ac8a01a35
DB <diego.botia@udea.edu.co>
2026-09-24 20:15:12 -0500

    Lab 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import  * as React from 'react';
 import './App.css';
+import CarList from './CarList';
+import logo from './logo.svg';
 
-function App() {
+
+class App extends React.Component<{},any>{
+
+  
+render(){
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
       </header>
+
+      <CarList/>
     </div>
   );
 }
-
+}
 export default App;
+
---
+++ src/CarList.tsx
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import logo from './logo.svg';
+
+interface Car {
+  id: number;
+  name: string;
+}
+
+const CarList: React.FC = () => {
+
+  const [cars, setCars] = useState<Car[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    fetch('http://localhost:8080/cool-cars')
+      .then(response => response.json())
+      .then(data => {
+        setCars(data);
+        setIsLoading(false);
+      });
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+      
+        <h1 className="App-title">Welcome to React</h1>
+      </header>
+
+      <div>
+        <h2>Car List</h2>
+        {cars.map((car: Car) => (
+          <div key={car.id}>
+            {car.name}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default CarList;
+
